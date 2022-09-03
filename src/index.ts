f773b472af62fb2444f174b13b6fbba2cd24bd8f
guyYablonka <guyyablonka@gmail.com>
2022-09-03 18:13:01 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import mongoose from "mongoose";
+
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+
+const app = express();
+app.use(json());
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:3001");
+    console.log("connected to mongoDB");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+app.listen(4000, () => {
+  console.log("Listening on port 4000!!!!!!!!");
+});
+
+start();
