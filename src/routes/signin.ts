35a64f09b89f8f8bf5d77ae113cef1573b316d2e
guyYablonka <guyyablonka@gmail.com>
2022-09-10 18:26:31 +0300

    add middleware for current user logic

--- src/routes/signin.ts
+++ src/routes/signin.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+
+import Password from "../services/password";
+import { BadRequestError } from "../errors/bad-request-error";
 import { validationRequest } from "../middlewares/validate-request";
+import { User } from "../models/user";
 const router = express.Router();
 
 router.post(
@@ -13,7 +18,37 @@ router.post(
       .withMessage("You must supply a password"),
   ],
   validationRequest,
-  (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    const passwordMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordMatch) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // Store it on session object
+    req.session = { jwt: userJwt };
+
+    res.status(200).send(existingUser);
+  }
 );
 
 export { router as signinRouter };
