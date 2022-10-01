405b751a434b7e695811b596675277ad74111c24
guyYablonka <guyyablonka@gmail.com>
2022-10-01 18:16:41 +0300

    change: some k8s configs and add skaffold

--- src/index.ts
+++ src/index.ts
@@ -27,8 +27,8 @@ const start = async () => {
   }
 };
 
-app.listen(4000, () => {
-  console.log("Listening on port 4000!!!!!!!!");
+app.listen(3000, () => {
+  console.log("Listening on port 3000!!!!!!!!");
 });
 
 start();
